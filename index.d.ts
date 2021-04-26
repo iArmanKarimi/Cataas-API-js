96acc3f3259e2ae6bcf941111a35d0d383350e14
iArmann <mrevid2@gmail.com>
2021-04-26 16:26:43 +0430

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,59 @@
+import { IncomingMessage } from "http"
+
+type Size =
+    | "or"
+    | "sq"
+    | "md"
+    | "sm"
+
+type Filter =
+    | "pixel"
+    | "paint"
+    | "mono"
+    | "blur"
+    | "sepia"
+    | "negative"
+
+interface Options {
+    Gif: boolean
+    Tag: string
+    Text: string
+    Width: number
+    Height: number
+    TextSize: number
+    TextColor: string
+    /** 
+    + sm : small
+    + md : medium
+    + sq : square
+    + or : original
+    */
+    Size: Size
+    Filter: Filter
+}
+
+interface GetCatsOptions {
+    Skip: number
+    Limit: number
+}
+
+interface CatByTag {
+    id: string
+    tags: string
+    created_at: string
+}
+
+class Cataas {
+    public options: Options
+
+    constructor(options?: Options)
+
+    public encode(): URL
+    public encodeById(id: string): URL
+    public get(): Promise<IncomingMessage>
+    public download(path: string): Promise<bool>
+    public getAllTags(): Promise<string[]>
+    public getCats(tags: string[], options?: GetCatsOptions): Promise<CatByTag[]>
+}
+
+export = Cataas
